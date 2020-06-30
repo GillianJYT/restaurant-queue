4f853d2fdb947ace98ade2163580614f5fd07591
GillianJYT <59901442+GillianJYT@users.noreply.github.com>
2020-06-30 16:19:36 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,307 @@
+namespace SpriteKind {
+    export const customer = SpriteKind.create()
+    export const buttonBlue = SpriteKind.create()
+    export const buttonOrange = SpriteKind.create()
+    export const buttonPink = SpriteKind.create()
+    export const server = SpriteKind.create()
+}
+namespace myTiles {
+    //% blockIdentity=images._tile
+    export const tile0 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile2 = img`
+. . f f f f f f f f f f f f f f 
+. f c c c c c c c c c c c c c c 
+f c c e e e e e e e e e e e e e 
+f c e e e 1 1 1 1 1 1 1 1 1 1 1 
+f c e e 1 e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+`
+    //% blockIdentity=images._tile
+    export const tile4 = img`
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e e 1 e e e e e e e e e e e 
+f c e e e 1 1 1 1 1 1 1 1 1 1 1 
+f c c e e e e e e e e e e e e e 
+. f c c c c c c c c c c c c c c 
+. . f f f f f f f f f f f f f f 
+`
+    //% blockIdentity=images._tile
+    export const tile5 = img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+`
+    //% blockIdentity=images._tile
+    export const tile6 = img`
+f f f f f f f f f f f f f f . . 
+c c c c c c c c c c c c c c f . 
+e e e e e e e e e e e e e c c f 
+1 1 1 1 1 1 1 1 1 1 1 e e e c f 
+e e e e e e e e e e e 1 e e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+`
+    //% blockIdentity=images._tile
+    export const tile7 = img`
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e 1 e e c f 
+1 1 1 1 1 1 1 1 1 1 1 e e e c f 
+e e e e e e e e e e e e e c c f 
+c c c c c c c c c c c c c c f . 
+f f f f f f f f f f f f f f . . 
+`
+    //% blockIdentity=images._tile
+    export const tile8 = img`
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+e e e e e e e e e e e e 1 e c f 
+`
+    //% blockIdentity=images._tile
+    export const tile9 = img`
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+f c e 1 e e e e e e e e e e e e 
+`
+}
+function numberOfQueues () {
+	
+}
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.buttonPink, function (sprite, location) {
+    numberOfServers()
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.buttonTeal, function (sprite, location) {
+    numberOfServers()
+})
+function queueStyle () {
+    if (controller.A.isPressed()) {
+        server2 = sprites.create(img`
+. . . f f f c c c . . . . . 
+. f f 5 5 5 5 5 5 f f . . . 
+. f 5 5 5 5 5 5 5 5 5 f . . 
+f 5 5 5 5 5 5 5 5 5 5 5 c . 
+f 5 5 b d 5 5 5 5 5 5 5 c . 
+f 5 d 4 4 b 5 5 5 5 5 5 5 f 
+f 5 b 4 4 4 c c 5 5 5 5 5 f 
+f f f 4 4 c b c b 5 5 5 b f 
+. f f d d c 1 e b b b b b c 
+. . f d d d d 4 f b b b b c 
+. . f 4 4 4 e e e 4 b b c . 
+. . f 9 9 9 e d d 4 f f . . 
+. . f 9 9 9 e d d e f . . . 
+. f 3 3 b 3 b e e b f . . . 
+. f f 3 b 3 b 3 b f f . . . 
+. . . f f b b f f . . . . . 
+`, SpriteKind.server)
+        server2.setPosition(120, 40)
+        queueBoolean = 2
+    }
+}
+function numberOfServers () {
+    if (server_list[2].image == img`
+. . . f f f c c c . . . . . 
+. f f 5 5 5 5 5 5 f f . . . 
+. f 5 5 5 5 5 5 5 5 5 f . . 
+f 5 5 5 5 5 5 5 5 5 5 5 c . 
+f 5 5 b d 5 5 5 5 5 5 5 c . 
+f 5 d 4 4 b 5 5 5 5 5 5 5 f 
+f 5 b 4 4 4 c c 5 5 5 5 5 f 
+f f f 4 4 c b c b 5 5 5 b f 
+. f f d d c 1 e b b b b b c 
+. . f d d d d 4 f b b b b c 
+. . f 4 4 4 e e e 4 b b c . 
+. . f 9 9 9 e d d 4 f f . . 
+. . f 9 9 9 e d d e f . . . 
+. f 3 3 b 3 b e e b f . . . 
+. f f 3 b 3 b 3 b f f . . . 
+. . . f f b b f f . . . . . 
+`) {
+    	
+    }
+}
+// don't know if these work
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.buttonOrange, function (sprite, location) {
+    queueStyle()
+})
+let queueBoolean = 0
+let server2: Sprite = null
+let server_list: Sprite[] = []
+let player1 = sprites.create(img`
+. . . . . . f f f f . . . . . . 
+. . . . f f f 2 2 f f f . . . . 
+. . . f f f 2 2 2 2 f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f f e 2 2 2 2 2 2 e e f . . 
+. . f e 2 f f f f f f 2 e f . . 
+. . f f f f e e e e f f f f . . 
+. f f e f b f 4 4 f b f e f f . 
+. f e e 4 1 f d d f 1 4 e e f . 
+. . f e e d d d d d d e e f . . 
+. . . f e e 4 4 4 4 e e f . . . 
+. . e 4 f 2 2 2 2 2 2 f 4 e . . 
+. . 4 d f 2 2 2 2 2 2 f d 4 . . 
+. . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`, SpriteKind.Player)
+player1.setPosition(142, 63)
+controller.moveSprite(player1)
+tiles.setTilemap(tiles.createTilemap(
+            hex`0a000800080808080801040808090808080808070608080a0808080808020508080b0808080808010408080808080808080706080808080808080802050808080808080808080808080808080808080808080808`,
+            img`
+. . . . . 2 2 . . . 
+. . . . . 2 2 . . . 
+. . . . . 2 2 . . . 
+. . . . . 2 2 . . . 
+. . . . . 2 2 . . . 
+. . . . . 2 2 . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+`,
+            [myTiles.tile0,myTiles.tile2,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7,myTiles.tile8,myTiles.tile9,sprites.dungeon.floorLight2,sprites.dungeon.buttonOrange,sprites.dungeon.buttonTeal,sprites.dungeon.buttonPink],
+            TileScale.Sixteen
+        ))
+scene.cameraFollowSprite(player1)
+let customer_list = sprites.allOfKind(SpriteKind.customer)
+server_list = sprites.allOfKind(SpriteKind.server)
+for (let index = 0; index <= 2; index++) {
+    server_list.insertAt(index, sprites.create(img`
+. . . f f f c c c . . . . . 
+. f f 5 5 5 5 5 5 f f . . . 
+. f 5 5 5 5 5 5 5 5 5 f . . 
+f 5 5 5 5 5 5 5 5 5 5 5 c . 
+f 5 5 b d 5 5 5 5 5 5 5 c . 
+f 5 d 4 4 b 5 5 5 5 5 5 5 f 
+f 5 b 4 4 4 c c 5 5 5 5 5 f 
+f f f 4 4 c b c b 5 5 5 b f 
+. f f d d c 1 e b b b b b c 
+. . f d d d d 4 f b b b b c 
+. . f 4 4 4 e e e 4 b b c . 
+. . f 9 9 9 e d d 4 f f . . 
+. . f 9 9 9 e d d e f . . . 
+. f 3 3 b 3 b e e b f . . . 
+. f f 3 b 3 b 3 b f f . . . 
+. . . f f b b f f . . . . . 
+`, SpriteKind.server))
+    server_list[index].setPosition(120, 15 + index * 25)
+}
+let index = 0
+// not random
+game.onUpdateInterval(Math.randomRange(500, 2000), function () {
+    customer_list.insertAt(index, sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . b 5 5 b . . . 
+. . . . . . b b b b b b . . . . 
+. . . . . b b 5 5 5 5 5 b . . . 
+. b b b b b 5 5 5 5 5 5 5 b . . 
+. b d 5 b 5 5 5 5 5 5 5 5 b . . 
+. . b 5 5 b 5 d 1 f 5 d 4 f . . 
+. . b d 5 5 b 1 f f 5 4 4 c . . 
+b b d b 5 5 5 d f b 4 4 4 4 b . 
+b d d c d 5 5 b 5 4 4 4 4 4 4 b 
+c d d d c c b 5 5 5 5 5 5 5 b . 
+c b d d d d d 5 5 5 5 5 5 5 b . 
+. c d d d d d d 5 5 5 5 5 d b . 
+. . c b d d d d d 5 5 5 b b . . 
+. . . c c c c c c c c b b . . . 
+`, SpriteKind.customer))
+    customer_list[index].setPosition(5, 15)
+    customer_list[index].setVelocity(50, 0)
+    index += 1
+})
